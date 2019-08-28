a9b5839360b3f7364f3963196a23a000d8fc9ce7
Arturo Guzman <ing.arturo.guzman@gmail.com>
2019-08-28 00:50:56 -0400

    Add get sites functions

---
+++ src/sites.ts
@@ -0,0 +1,11 @@
+import { ListSitesResponse } from './types/helpscout-docs'
+import { get } from './request'
+
+interface ListSitesOptions {
+  page?: number
+}
+
+export async function listSites(apiToken: string, options?: ListSitesOptions) {
+  const response = await get<ListSitesResponse>(apiToken, '/sites', options)
+  return response.body
+}
