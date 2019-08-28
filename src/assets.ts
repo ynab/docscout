c83694fa4977622d064c55fc0ddbfaea7cea9838
Arturo Guzman <ing.arturo.guzman@gmail.com>
2019-08-28 00:52:20 -0400

    Improve assets api

--- src/assets.ts
+++ src/assets.ts
@@ -1,33 +1,47 @@
-import * as FormData from 'form-data'
+import FormData from 'form-data'
 import * as fs from 'fs'
 import { basename } from 'path'
 import { post } from './request'
 import { CreateArticleAssetResponse } from './types/helpscout-docs'
+import { Stream } from 'stream'
 
 type AssetTypeParameter = 'image' | 'attachment'
 
-interface CreateArticleAssetOption {
+interface CreateArticleAssetOptionsBase {
   key: string
   articleId: string
   assetType: AssetTypeParameter
   fileName?: string
 }
 
-export async function createArticleAsset(
-  apiToken: string,
-  sourceFilepath: string,
-  options: CreateArticleAssetOption
-) {
+type CreateArticleAssetOptionWithFilepath = CreateArticleAssetOptionsBase & {
+  filepath: string
+}
+
+type CreateArticleAssetOptionsWithStream = CreateArticleAssetOptionsBase & {
+  stream: Stream
+}
+
+export type CreateArticleAssetOptions =
+  | CreateArticleAssetOptionWithFilepath
+  | CreateArticleAssetOptionsWithStream
+
+export async function createArticleAsset(apiToken: string, options: CreateArticleAssetOptions) {
   const form = new FormData()
-  form.append('key', this.apiToken)
+  form.append('key', apiToken)
   form.append('articleId', options.articleId)
   form.append('assetType', options.assetType)
-  form.append('file', fs.createReadStream(sourceFilepath), basename(sourceFilepath))
+  if ('filepath' in options) {
+    form.append('file', fs.createReadStream(options.filepath), basename(options.filepath))
+  } else if ('stream' in options) {
+    form.append('file', options.stream)
+  }
+
   if (options.fileName) {
     form.append('fileName', options.fileName)
   }
 
-  const response = await post<CreateArticleAssetResponse>(apiToken, this.url(`/assets/article`), {
+  const response = await post<CreateArticleAssetResponse>(apiToken, `/assets/article`, {
     body: form,
     headers: form.getHeaders()
   })
