1b53db309655d70fd2cec7cf020a6f2ec55a2f24
Arturo Guzman <ing.arturo.guzman@gmail.com>
2019-08-28 00:52:57 -0400

    Updating test run script

--- scripts/test-run.ts
+++ scripts/test-run.ts
@@ -1,14 +1,43 @@
-import * as collections from '../src/collections'
+import * as lodash from 'lodash'
+import DocsClient from '../src'
 
 const apiToken = process.env.HS_DOCS_API_TOKEN
 
 if (apiToken) {
-  collections
-    .listCollections(apiToken)
-    .then(collectionsResponse => {
-      console.log(JSON.stringify(collectionsResponse, null, 2))
-    })
-    .catch(error => {
+  ;(async () => {
+    try {
+      const client = new DocsClient(apiToken)
+      const publicCollectionsEnvelope = await client.listCollections({ visibility: 'public' })
+      const publicCollections = publicCollectionsEnvelope.items
+      const firstPublicCollection = publicCollections[0]
+      printObject(firstPublicCollection)
+      const articlesEnvelope = await client.listArticlesInCollection(firstPublicCollection.id, {
+        status: 'published',
+        sort: 'updatedAt',
+        order: 'desc'
+      })
+
+      console.log(`Found ${articlesEnvelope.count} in ${articlesEnvelope.pages} pages`)
+      const mostRecentlyUpdatedArticleRef = articlesEnvelope.items[0]
+      printObject(mostRecentlyUpdatedArticleRef)
+
+      const mostRecentlyUpdatedArticle = await client.getArticle(mostRecentlyUpdatedArticleRef.id)
+      printObject(mostRecentlyUpdatedArticle)
+
+      const revisions = await client.listRevisions(mostRecentlyUpdatedArticle.id)
+      printObject(revisions)
+
+      const oldestRevision = lodash.last(revisions.items)
+      if (oldestRevision) {
+        const revision = await client.getRevision(oldestRevision.id)
+        printObject(revision)
+      }
+    } catch (error) {
       console.error(error)
-    })
+    }
+  })()
+}
+
+function printObject(data: any) {
+  console.log(JSON.stringify(data, null, 2))
 }
