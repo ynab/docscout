1e2b9813e2bd4e4a427288705bd189cfb6cc2159
Arturo Guzman <ing.arturo.guzman@gmail.com>
2019-08-28 00:52:07 -0400

    Updating categories

--- src/categories.ts
+++ src/categories.ts
@@ -1,4 +1,4 @@
-import { OrderParameter, ListCategoriesResponse, GetCategoryResponse } from './types/helpscout-docs'
+import { OrderParameter, ListCategoriesResponse, GetCategoryResponse, Category } from './types/helpscout-docs'
 import { get } from './request'
 
 type CategoriesSortParameter = 'number' | 'order' | 'name' | 'articleCount' | 'createdAt' | 'updatedAt'
@@ -17,6 +17,28 @@ export async function listCategories(
   const path = `/collections/${collectionId}/categories`
   const response = await get<ListCategoriesResponse>(apiToken, path, options)
   return response.body
+}
+
+interface ListAllCategoriesOptions {
+  sort?: CategoriesSortParameter
+  order?: OrderParameter
+}
+
+async function listAllCategories(apiToken: string, collectionId: string, options?: ListAllCategoriesOptions) {
+  const categories: Category[] = []
+  const baseOptions = Object.assign({}, options)
+
+  let currentPage = 1
+  let totalPages = 1
+  while (currentPage <= totalPages) {
+    const currentOptions = Object.assign({}, baseOptions, { page: currentPage })
+    const categoriesList = await listCategories(apiToken, collectionId, currentOptions)
+    categories.splice(-1, 0, ...categoriesList.categories.items)
+    totalPages = categoriesList.categories.pages
+    currentPage += 1
+  }
+
+  return categories
 }
 
 export async function getCategory(apiToken: string, categoryIdOrNumber: string | number) {
