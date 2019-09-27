0cc0f4dc3068afe4795cbee9ff8adb09c0a8c1dc
Arturo Guzman <ing.arturo.guzman@gmail.com>
2019-09-27 19:45:51 -0400

    Add missing category properties

--- src/types/helpscout-docs/categories.d.ts
+++ src/types/helpscout-docs/categories.d.ts
@@ -1,13 +1,21 @@
-import { DateTime, ItemsCollectionEnvelope } from './common'
+import { DateTime, ItemsCollectionEnvelope, Visibility } from './common'
+
+export type CategorySort = 'popularity' | 'name' | 'updatedAt' | 'custom'
 
 // https://developer.helpscout.com/docs-api/objects/category/
 export interface Category {
   id: string
   number: number
   slug: string
+  visibility: Visibility
   collectionId: string
   order: number
+  defaultSort: CategorySort
   name: string
+  description: string
+  articleCount: number
+  publishedArticleCount: number
+  publicUrl: string
   createdBy: number
   updatedBy: number
   createdAt: DateTime
